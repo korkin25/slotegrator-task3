84af9a550a20dadf2328373db340cac08e5f4238
korkin25 <eugeny.kuzakov@gmail.com>
2024-01-06 09:46:47 +0800

    VM setup refactoring: one simple j2 template

--- k8s-bootstrap/yaml2terraform.py
+++ k8s-bootstrap/yaml2terraform.py
@@ -1,37 +1,17 @@
+#!/usr/bin/env python3
 import yaml
 from jinja2 import Environment, FileSystemLoader
 import pprint
 
-
 def generate_terraform_file(config, template_env, output_file='main.tf'):
     with open(output_file, 'w') as file:
-        # Get the SSH key path
-        ssh_key = config.get('ssh_key', '')
+        # Determine the template file name based on the provider name
+        provider_name = config['virtual_machines_config']['global']['required_provider']
+        template_file_name = f'vm_{provider_name}.j2'
 
-        # Process provider information
-        vm_global_config = config['virtual_machines_config']['global']
-        vm_providers_info = vm_global_config['required_providers']
-        vm_provider_config = template_env.get_template('provider_template.j2').render(providers=vm_providers_info)
-        file.write(vm_provider_config)
-
-        # Generate VM configurations
-        vms_config = config['virtual_machines_config']['virtual_machines']
-        for vm in vms_config:
-            for vm_type in vm.keys():
-                print(f"Generating configuration for VM type: {vm_type}:\n")
-                pprint.pprint(vm_type)
-
-                for i in range(vm_type.counts):
-                    vm_name = f"{vm.get('name', vm_type)}-{i}"
-                    terraform_config = template_env.get_template('vm_template.j2').render(
-                        vm=vm, vm_name=vm_name, ssh_key=ssh_key
-                    )
-                    file.write(terraform_config)
-
-        # Add output blocks
-        output_config = template_env.get_template('output_template.j2').render(vms=vms_config)
-        file.write(output_config)
-
+        # Render the template with the entire config
+        rendered_config = template_env.get_template(template_file_name).render(config=config)
+        file.write(rendered_config)
 
 def main(config_path='config.yaml'):
     try:
@@ -44,11 +24,13 @@ def main(config_path='config.yaml'):
         print(f"Error opening configuration file: {str(e)}")
         return
 
+    print("Configuration file:")
+    pprint.pprint(config)
+
     template_loader = FileSystemLoader(searchpath='./')
     template_env = Environment(loader=template_loader)
 
     generate_terraform_file(config, template_env)
-
 
 if __name__ == "__main__":
     import sys
